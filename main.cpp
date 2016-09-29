b7a05aa281b29b7b77f8c2aa2070f0c6a5e44a93
admin <anton_vr@mail.ru>
2016-09-29 23:46:57 +0400

    raznost

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int main()
 {
- int num1,num2,summ;
+ int num1,num2,summ,razn;
  cout << "enter integer numbers: ";
     cin >> num1;
       cin >> num2;
       summ=num1+num2;
 cout<<summ;
+razn=num1+num2;
+cout<<razn;
+
 }
