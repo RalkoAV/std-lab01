4cb7f61ef5296d071425be6962aa27bc6c63ff88
admin <anton_vr@mail.ru>
2016-09-29 23:59:46 +0400

    umnojenie

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // ÈÒÑ Ó×ÅÁÍÀß ÏĞÎÃĞÀÌ
 int main()
 {
- int num1,num2,summ,razn;
+ int num1,num2,summ,razn,umnoj;
  cout << "enter integer numbers: ";
     cin >> num1;
       cin >> num2;
@@ -12,5 +12,6 @@ int main()
 cout<<summ;
 razn=num1+num2;
 cout<<razn;
-
+umnoj=num1*num2;
+cout<<umnoj;
 }
