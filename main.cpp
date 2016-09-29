cd367033afae6f9908161e5d6240811ff13a7854
admin <anton_vr@mail.ru>
2016-09-29 23:03:28 +0400

    summachast1

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main()
 {
- int num1,num2;
+ int num1,num2,summ;
  cout << "enter integer numbers: ";
     cin >> num1;
       cin >> num2;
+      summ=num1+num2;
+cout<<summ;
 }
